15844798bb663f3921f699bf668a373231ae4ec4
tony-tsx <tony.js@zoho.eu>
2020-01-30 12:52:36 -0300

    Redução significativa do código

--- src/components/Block.tsx
+++ src/components/Block.tsx
@@ -1,23 +1,12 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { View, ViewProps } from 'react-native'
 import Style from '../utils/Style'
 import Event from '../utils/Event'
 
-class Block extends React.Component<Block.Props> {
-  private parser = () => {
-    const { children, on, ...rest } = this.props
-
-    const { style, props } = Style.parser( rest )
-
-    if ( on ) Object.assign( props, Event.parsers.view( on ).events )
-
-    return { style, props, children }
-  }
-  public render = () => {
-    const { props, style, children } = this.parser()
-
-    return <View { ...props } style={ style }>{children}</View>
-  }
+const Block: React.StatelessComponent<Block.Props> = ( { children, on, ...rest } ) => {
+  const { style, props } = Style.parser( rest )
+  if ( on ) Object.assign( props, Event.parsers.view( on ).events )
+  return <View { ...props } style={ style }>{ children }</View>
 }
 
 namespace Block {
