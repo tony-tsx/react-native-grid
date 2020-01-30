39cb992dd3d47e5150aa73e667b03733f3447e4b
tony-tsx <tony.js@zoho.eu>
2020-01-30 13:03:20 -0300

    Troca de diretorio dos testes unitarios

--- src/__TESTS__/col.spec.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Row } from '..'
-import { create } from 'react-test-renderer'
-
-test( 'Component Row', () => {
-  const component = create( <Row /> )
-  console.log( component.toJSON() )
-} )
